e717795575a3455987c7493baafe5ed80ab326f9
Saad Amir <samir.bscs18seecs@student.nust.edu.pk>
2022-02-25 18:02:21 +0500

    initializing repo

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: "" };
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.lat && !this.state.errorMessage) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error : {this.state.errorMessage}</div>;
+    }
+
+    return <Spinner message="Please Allow to use your location" />;
+  }
+  render() {
+    return <div>{this.renderContent()}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
